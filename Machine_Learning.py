184ed8ff21850511d8cbfc99f7c02987918f01e5
felipeslisboa99 <felipeslisboa99@gmail.com>
2025-02-07 22:14:01 -0300

    Update Machine_Learning.py

--- Machine_Learning.py
+++ Machine_Learning.py
@@ -14,18 +14,18 @@ from sklearn.metrics import (
 
 )
 from statsmodels.tsa.seasonal import seasonal_decompose
+def app():
+    df = pd.read_excel("base_petroleo.xlsx")
+    df.head()
 
-df = pd.read_excel("base_petroleo.xlsx")
-df.head()
-
-df = pd.DataFrame({
-    "data": df['data'],
-    "valor": df['preco_bruto_Brent_FOB']
+    df = pd.DataFrame({
+        "data": df['data'],
+        "valor": df['preco_bruto_Brent_FOB']
 })
-df_mensal = df.groupby(df["data"].dt.to_period("M")).agg({"valor": "mean"}).reset_index()
-df_mensal.rename(columns={"data": "mes", "valor": "mean_mensal"}, inplace=True)
-df_anual = df.groupby(df["data"].dt.to_period("Y")).agg({"valor": "mean"}).reset_index()
-df_anual.rename(columns={"data": "ano", "valor": "media_anual"}, inplace=True)
-df['data'] = pd.to_datetime(df["data"])
-df.dtypes
+    df_mensal = df.groupby(df["data"].dt.to_period("M")).agg({"valor": "mean"}).reset_index()
+    df_mensal.rename(columns={"data": "mes", "valor": "mean_mensal"}, inplace=True)
+    df_anual = df.groupby(df["data"].dt.to_period("Y")).agg({"valor": "mean"}).reset_index()
+    df_anual.rename(columns={"data": "ano", "valor": "media_anual"}, inplace=True)
+    df['data'] = pd.to_datetime(df["data"])
+    df.dtypes
 
