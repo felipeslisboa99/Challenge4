a8f591be48669b6ff62fe07a9992748eff1226a5
felipeslisboa99 <felipeslisboa99@gmail.com>
2025-02-07 22:19:11 -0300

    Update Machine_Learning.py

--- Machine_Learning.py
+++ Machine_Learning.py
@@ -28,4 +28,26 @@ def app():
     df_anual.rename(columns={"data": "ano", "valor": "media_anual"}, inplace=True)
     df['data'] = pd.to_datetime(df["data"])
     df.dtypes
+#Serie Temporal
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['data'], df['valor'], linestyle='-')
+    plt.title('Série Temporal', fontsize=14)
+    plt.xlabel('Data', fontsize=12)
+    plt.ylabel('Valor', fontsize=12)
+    plt.grid(True, which='major', axis='x', linestyle='--', color='gray', alpha=0.7)
+    plt.xticks(pd.date_range(start='2004-01-01', end='2024-12-31', freq='YS'), rotation=45)
+    plt.tight_layout()
+    plt.show()
+    
+    result = seasonal_decompose(df['valor'], model='additive', period=365)
+    fig = result.plot()
+    fig.set_size_inches(14,7)
+#Serie Temporal Diferenciada
+    df.loc[:, 'Diferenciado'] = df['valor'].diff()
 
+    plt.figure(figsize=(12, 6))
+    plt.plot(df['Diferenciado'])
+    plt.title('Série Temporal Diferenciada (Estacionarizada)')
+    plt.xlabel('Tempo')
+    plt.ylabel('Valores Diferenciados')
+    plt.show()
